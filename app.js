ea4043b3a480ef5d96c851f5446a4bb844083bb0
Nikhil-sha <nikhil.sharma.main@gmail.com>
2025-02-07 18:46:03 +0530

    update app.js

--- app.js
+++ app.js
@@ -30,30 +30,28 @@ class App extends Component {
 					})
 			})
 	}
-	
+
 	render() {
 		return (
-			<React.StrictMode>
-				<HashRouter>
-					<Header />
+			<HashRouter>
+				<Header />
 
-					<main className="grow min-h-0 flex flex-col gap-4 max-w-xl w-full px-4 py-4 mx-auto overflow-y-auto bg-gray-900 text-gray-200">
-						<Switch>
-							<Route path="/edit/:padId" component={Edit} />
-							<Route path="/view/:padId" component={View} />
-							<Route exact path="/dashboard" component={Dashboard} />
-							<Route exact path="/github" component={GitHub} />
-							<Route exact path="/delete" component={Delete} />
-							<Route exact path="/new" component={New} />
-							<Route exact path="/menu" component={Menu} />
-							<Route exact path="/" component={Home} />
-							<Route component={NotFound} />
-						</Switch>
-					</main>
+				<main className="grow min-h-0 flex flex-col gap-4 max-w-xl w-full px-4 py-4 mx-auto overflow-y-auto bg-gray-900 text-gray-200">
+					<Switch>
+						<Route path="/edit/:padId" component={Edit} />
+						<Route path="/view/:padId" component={View} />
+						<Route exact path="/dashboard" component={Dashboard} />
+						<Route exact path="/github" component={GitHub} />
+						<Route exact path="/delete" component={Delete} />
+						<Route exact path="/new" component={New} />
+						<Route exact path="/menu" component={Menu} />
+						<Route exact path="/" component={Home} />
+						<Route component={NotFound} />
+					</Switch>
+				</main>
 
-					<BottomNav />
-				</HashRouter>
-			</React.StrictMode>
+				<BottomNav />
+			</HashRouter>
 		);
 	}
 }
