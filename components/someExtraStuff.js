644f57ad837afb65eba2811f0ba0490a1b329d8c
Nikhil-sha <nikhil.sharma.main@gmail.com>
2025-02-09 13:29:47 +0530

    update someExtraStuff.js

--- components/someExtraStuff.js
+++ components/someExtraStuff.js
@@ -19,28 +19,28 @@ from **Nikhil Sharma**`;
 export const purifyConfig = {
 	// Allow safe tags for text formatting, images, lists, tables, and code blocks
 	ALLOWED_TAGS: [
-    "a", "b", "i", "em", "strong", "u", "p", "br", "hr", "blockquote",
-    "code", "pre", "span", "div", "ul", "ol", "li", "dl", "dt", "dd",
-    "table", "thead", "tbody", "tfoot", "tr", "th", "td", "img", "figure", "figcaption",
+  "a", "b", "i", "em", "strong", "u", "p", "br", "hr", "blockquote",
+  "code", "pre", "span", "div", "ul", "ol", "li", "dl", "dt", "dd",
+  "table", "thead", "tbody", "tfoot", "tr", "th", "td", "img", "figure", "figcaption",
 
-    // Headings
-    "h1", "h2", "h3", "h4", "h5", "h6",
+ 	// Headings
+ 	"h1", "h2", "h3", "h4", "h5", "h6",
 
-    // Media elements
-    "audio", "video", "source", "iframe",
+  // Media elements
+  "audio", "video", "source", "iframe",
 
-    // Semantic elements
-    "mark", "small", "sub", "sup", "del", "ins", "kbd"
-  ],
+  // Semantic elements
+  "mark", "small", "sub", "sup", "del", "ins", "kbd"
+ ],
 
 	// Allow necessary attributes for functionality while preventing JavaScript execution
 	ALLOWED_ATTR: [
-    "href", "target", "rel", // Links
-    "src", "alt", "title", "width", "height", // Images & media
-    "controls", "autoplay", "loop", "muted", "poster", // Audio & video
-    "frameborder", "allowfullscreen", "sandbox", // iFrame security
-    "colspan", "rowspan", "align", "border", "cellpadding", "cellspacing" // Table attributes
-  ],
+  "href", "target", "rel", // Links
+  "src", "alt", "title", "width", "height", // Images & media
+  "controls", "autoplay", "loop", "muted", "poster", // Audio & video
+  "frameborder", "allowfullscreen", "sandbox", // iFrame security
+  "colspan", "rowspan", "align", "border", "cellpadding", "cellspacing" // Table attributes
+ ],
 
 	// Allow embedding but restrict external content to known safe sources (e.g., YouTube, Vimeo)
 	ALLOWED_URI_REGEXP: /^(https?:\/\/(?:www\.)?(youtube\.com|youtu\.be|vimeo\.com|soundcloud\.com)(\/.*)?$)/,
